01b3e117e43c0b24e96e28eec420712165e477fd
Chethiya2001 <chethiyabandaraherath@gmail.com>
2026-09-24 11:08:56 +0530

    fixed issue

--- src/automation/automation.module.ts
+++ src/automation/automation.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { AutomationController } from './automation.controller';
 import { AutomationService } from './automation.service';
 import { AutomationFlow } from './entities/automation-flow.entity';
 
-@Module({ imports: [TypeOrmModule.forFeature([AutomationFlow])], controllers: [AutomationController], providers: [AutomationService] })
+@Module({
+	imports: [AuthModule, TypeOrmModule.forFeature([AutomationFlow])],
+	controllers: [AutomationController],
+	providers: [AutomationService],
+})
 export class AutomationModule {}
